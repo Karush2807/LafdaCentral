e20e1af9e14a5d851eaa3d584822bd887b6dec15
Karush2807 <arushkarnatak1881@gmail.com>
2024-11-30 03:31:53 +0530

    Feature03: startted workign on profile section

--- lafdaCentre/mudda/models.py
+++ lafdaCentre/mudda/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 # Create your models here.
 
 class Mudda(models.Model):
@@ -35,3 +37,21 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"{self.user.username} commented on {self.mudda}"
+    
+
+#for profile section
+class Profile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    pfp=models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    bio=models.TextField(max_length=500, blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
